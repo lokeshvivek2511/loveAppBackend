d1f7682b733beefbdd9cc92b1e35a27d731136e3
lokeshvivek2511 <149708261+lokeshvivek2511@users.noreply.github.com>
2025-03-02 02:58:53 +0530

    Update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -18,6 +18,9 @@ const corsOptions = {
   optionsSuccessStatus: 204,
 };
 app.use(cors(corsOptions));
+
+app.use(cors({ origin: "*", credentials: true }));
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
